efdece3c9840160246b51918d6c1e34f5500f924
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2025-06-10 10:26:03 -0700

    Add files via upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { Canvas } from '@threlte/core';
+	import Scene from './Scene.svelte';
+</script>
+
+<Canvas>
+	<Scene />
+</Canvas>
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		background-color: #08010a;
+		color: #fff;
+	}
+</style>
